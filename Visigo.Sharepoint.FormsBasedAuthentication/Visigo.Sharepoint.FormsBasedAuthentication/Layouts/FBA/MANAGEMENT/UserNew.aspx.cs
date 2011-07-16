6fa8bafb8db8e805ee51f82e4a70a6af83a80f41
ccoulson <ccoulson@localhost>
2011-07-16 22:09:42 +0000

    Issue # 318: Fixed New User page to display a message instead of failing to an error page if the user fails to be created (due to bad password, duplicate email....)

--- Visigo.Sharepoint.FormsBasedAuthentication/Visigo.Sharepoint.FormsBasedAuthentication/Layouts/FBA/MANAGEMENT/UserNew.aspx.cs
+++ Visigo.Sharepoint.FormsBasedAuthentication/Visigo.Sharepoint.FormsBasedAuthentication/Layouts/FBA/MANAGEMENT/UserNew.aspx.cs
@@ -26,6 +26,8 @@ namespace Visigo.Sharepoint.FormsBasedAuthentication
 
             bool _showRoles = (new MembershipSettings(SPContext.Current.Web)).EnableRoles;
 
+            ReqValEmailSubject.Enabled = emailUser.Checked;
+
             if (!Page.IsPostBack)
             {
                 try
@@ -97,6 +99,19 @@ namespace Visigo.Sharepoint.FormsBasedAuthentication
                         user = Membership.CreateUser(txtUsername.Text, txtPassword.Text, txtEmail.Text, null, null, isActive.Checked, null, out createStatus);
                     }
 
+
+                    if (createStatus != MembershipCreateStatus.Success)
+                    {
+                        lblMessage.Text = GetErrorMessage(createStatus);
+                        return;
+                    }
+
+                    if (user == null)
+                    {
+                        lblMessage.Text = "An unknown error occurred. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
+                        return;
+                    }
+
                     bool groupAdded = false;
 
                     if (_showRoles)
@@ -160,6 +175,42 @@ namespace Visigo.Sharepoint.FormsBasedAuthentication
             else
             {
                 lblMessage.Text = "User Already Exists";
+            }
+        }
+
+        protected string GetErrorMessage(MembershipCreateStatus status)
+        {
+            switch (status)
+            {
+                case MembershipCreateStatus.DuplicateUserName:
+                    return "Username already exists. Please enter a different user name.";
+
+                case MembershipCreateStatus.DuplicateEmail:
+                    return "A username for that e-mail address already exists. Please enter a different e-mail address.";
+
+                case MembershipCreateStatus.InvalidPassword:
+                    return "The password provided is invalid. Please enter a valid password value.";
+
+                case MembershipCreateStatus.InvalidEmail:
+                    return "The e-mail address provided is invalid. Please check the value and try again.";
+
+                case MembershipCreateStatus.InvalidAnswer:
+                    return "The password retrieval answer provided is invalid. Please check the value and try again.";
+
+                case MembershipCreateStatus.InvalidQuestion:
+                    return "The password retrieval question provided is invalid. Please check the value and try again.";
+
+                case MembershipCreateStatus.InvalidUserName:
+                    return "The user name provided is invalid. Please check the value and try again.";
+
+                case MembershipCreateStatus.ProviderError:
+                    return "The authentication provider returned an error. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
+
+                case MembershipCreateStatus.UserRejected:
+                    return "The user creation request has been canceled. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
+
+                default:
+                    return "An unknown error occurred. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
             }
         }
 
